dd42aa20a319fb90008fe6ec83b2fefb281b0d2f
Ketan Kulkarni <ketan@carvingit.com>
2025-05-07 11:03:44 +0530

    bugfix

--- src/LaravelUserRolesServiceProvider.php
+++ src/LaravelUserRolesServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 namespace CarvingIT\LaravelUserRoles;
 
+use Illuminate\Support\ServiceProvider;
+
 class LaravelUserRolesServiceProvider extends ServiceProvider{
     public function boot(){
         $this->publishes([ __DIR__.'/database/migrations' => database_path('migrations')], 'migration');
