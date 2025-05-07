c8d4566b00987dd0a158bbb5bee2d3d6931fb7fa
Ketan Kulkarni <ketan@carvingit.com>
2025-05-07 10:59:47 +0530

    fixes

--- src/LaravelUserRolesServiceProvider.php
+++ src/LaravelUserRolesServiceProvider.php
@@ -3,8 +3,8 @@ namespace CarvingIT\LaravelUserRoles;
 
 class LaravelUserRolesServiceProvider extends ServiceProvider{
     public function boot(){
-        $this->publishes([ __DIR__.'/database/migrations' => database_path('migrations')], 'laravel-assets');
-        $this->publishes([ __DIR__.'/database/seeders' => database_path('seeders')], 'laravel-assets');
+        $this->publishes([ __DIR__.'/database/migrations' => database_path('migrations')], 'migration');
+        $this->publishes([ __DIR__.'/database/seeders' => database_path('seeders')], 'seeder');
     }
     public function register(){
     }
